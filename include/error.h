734ebeb094c936752395f057d9b19a4dac5367e0
dimy20 <enzostrindade16@outlook.com>
2022-03-07 01:52:54 -0300

    new error code and error strings mappings

--- include/error.h
+++ include/error.h
@@ -1,10 +1,18 @@
 #ifndef ERROR_H
 #define ERROR_H
 
+#define THROW_ERROR(err)  do { \
+	fprintf(stderr, "Error in %s:%d: %s.\n", __FILE__, __LINE__, errstr(err)); \
+	exit(EXIT_FAILURE); } while(0)
+
+#define LOG_ERROR(err) do { \
+	fprintf(stderr, "Error: %s:%d: %s.\n", __FILE__, __LINE__, errstr(err)); \
+} while(0)
+
 void error_log(char * msg);
 void error_exit(int ret, char * msg);
 void error_malloc(void * mem);
 void error_alert(int ret, char * msg);
-
+const char * errstr(unsigned int err_code);
 
 #endif 
