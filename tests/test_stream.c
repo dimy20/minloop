ebf885bbda56e50475917bb04ffe21cfab6d0d7a
dimy20 <enzostrindade16@outlook.com>
2022-03-08 23:11:59 -0300

    stream tests

--- tests/test_stream.c
+++ tests/test_stream.c
@@ -3,17 +3,36 @@
 #include "../include/stream.h"
 #include "../include/loop.h"
 
+void on_connection(stream_t * server){
+	printf("undefined\n");
+}
+
 void test_stream_init(void){
 	loop_t loop;
 	stream_t server;
 
 	loop_init(&loop);
-	TEST_ASSERT(stream_init(&loop, &server) == OP_SUCCESS);
-
+	TEST_ASSERT(stream_init(&server) == OP_SUCCESS);
 	TEST_ASSERT(stream_server(&loop, &server, "localhost", "8080") == OP_SUCCESS);
 }
 
+void test_stream_listen(void){
+	loop_t loop;
+	loop_init(&loop);
+
+	stream_t server;
+	int err;
+
+	stream_init(&server);
+	stream_server(&loop, &server, "localhost", "8080");
+
+	err = stream_listen(&loop, &server, NULL);
+	TEST_ASSERT_(err == -EINVAL, "connection callback cant be NULL");
+}
+
+
 TEST_LIST = {
 	{"void stream_init(loop_t * loop)", test_stream_init},
+	{"int stream_listen(loop_t *, stream_t *, connection_cb)", test_stream_listen},
     {0}
 };
