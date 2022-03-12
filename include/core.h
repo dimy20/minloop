3e60c8f56fafc3263f59e95a8d88dac8065ef227
dimy20 <enzostrindade16@outlook.com>
2022-03-12 17:11:54 -0300

    cb takes loop as arg, removed get/set

--- include/core.h
+++ include/core.h
@@ -10,8 +10,13 @@
 /*status or options*/
 #define STS_LISTEN 0x01
 
+#define IOCTL_ADD 1
+#define IOCTL_DEL 2
+
+typedef struct loop_s loop_t;
+
 typedef struct io_core_s io_core_t;
-typedef void(*io_core_cb)(io_core_t * ioc, uint8_t status);
+typedef void(*io_core_cb)(loop_t * loop, io_core_t * ioc, uint8_t status);
 
 struct io_core_s {
     int fd;
@@ -23,8 +28,7 @@ struct io_core_s {
 
 void io_core_init(io_core_t * ioc, int fd, uint32_t events, io_core_cb cb);
 int iocore_getfd(const io_core_t * ioc);
-void iocore_setfd(io_core_t * ioc, int fd);
-int _io_accept(io_core_t * io);
+int ioctl(io_core_t * ioc, int op, uint8_t epoll_ev);
 
 #endif
 
--- src/core.c
+++ src/core.c
@@ -18,23 +18,27 @@ void io_core_init(io_core_t * ioc, int fd, uint32_t events, io_core_cb cb){
 
 int _io_accept(io_core_t * io){
 	assert(io != NULL && "io_core_t pointer is NULL");
-	assert( iocore_getfd(io) >= 0 );
+	assert( io->fd >= 0 );
 
 	int err;
-	err = accept(iocore_getfd(io), NULL, NULL);
+	err = accept( io->fd, NULL, NULL);
 	if(err < 0) return -EIO_ACCEPT;
 
 	return err;
 };
 
-int iocore_getfd(const io_core_t * ioc){
-	assert(ioc != NULL && "io_core_t pointer is NULL");
-	return ioc->fd;
-}
+int ioctl(io_core_t * io, int op, uint8_t epoll_ev){
+	assert(io != NULL && "io_core_t pointer is NULL");
+	int err;
+	err = -EIO_INVAL_EV;
+	if( op == IOCTL_ADD ){
+		io->events |= epoll_ev;
+		err = OP_SUCCESS;
+	}else if (op == IOCTL_DEL){
+		io->events &= ~epoll_ev;
+		err = OP_SUCCESS;
+	}
 
-void iocore_setfd(io_core_t * ioc, int fd){
-	assert(ioc != NULL && "io_core_t pointer is NULL");
-	ioc->fd = fd;
+	return err;
 }
-
 
